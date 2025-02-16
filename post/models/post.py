381c1acdfcdda3725b0c91582f83ed2a4863c7d7
Shohruh <bekmurodovshohruh0224@gmail.com>
2025-02-16 12:47:41 +0500

    Created Manager

--- post/models/post.py
+++ post/models/post.py
@@ -6,7 +6,9 @@ class Status(models.TextChoices):
     DRAFT = 'DF', 'Draft'
     PUBLISHED = 'PB', 'Published'
 
-
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Status.PUBLISHED)
 
 class Post(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
@@ -17,6 +19,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=Status)
+    published = PublishedManager()
 
     def __str__(self):
         return f"{self.title} by {self.author}"
